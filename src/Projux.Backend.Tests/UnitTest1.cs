4b16a438859fcc81d59e5584d5ac9d1051bffdb0
NJKWilson <NJKWilson@outlook.com>
2022-07-18 22:56:56 +0100

    Fixed test projects and added new solution file

---
+++ src/Projux.Backend.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Projux.Backend.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/Projux.Frontend.Wpf.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Projux.Frontend.Wpf.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}

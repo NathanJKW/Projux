70d8aafee93014ba94683be8017cea8bd5c4216a
NJKWilson <NJKWilson@outlook.com>
2022-07-18 21:39:21 +0100

    added test projects

---
+++ src/Projux.Frontend.Wpf.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Projux.Frontend.Wpf.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

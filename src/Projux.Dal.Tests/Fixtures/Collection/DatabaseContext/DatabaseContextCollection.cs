aff91cd10bbfe0313af92249e8e66154e0ccccdc
NJKWilson <NJKWilson@outlook.com>
2022-07-18 22:05:12 +0100

    building tests out

---
+++ src/Projux.Dal.Tests/Fixtures/Collection/DatabaseContext/DatabaseContextCollection.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projux.Dal.Tests.Fixtures.Collection.DatabaseContext;
+
+[CollectionDefinition("DatabaseContextCollection")]
+public class DatabaseContextCollection : ICollectionFixture<DatabaseContextFixture>
+{
+    
+}
